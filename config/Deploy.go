e0026d1f3e51f0ecd19c31a47f4d7bfe79c933b1
panda1079 <33449950+panda1079@users.noreply.github.com>
2024-01-30 13:58:11 +0800

    Update Deploy.go

--- config/Deploy.go
+++ config/Deploy.go
@@ -14,7 +14,7 @@ func (r *Deploy) Run() map[string]string {
 	Month := time.Now().Format("01")
 	return map[string]string{
 		"LISTEN_ADDRESS": "0.0.0.0",
-		"PORT":           "9000",
+		"PORT":           "9091",
 		"LOG_DIR":        "log/server-" + Year + "-" + Month + ".log", //日志写入位置
 	}
 }
